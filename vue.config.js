e9773f0d9573aca0ba2167f988fbfe2df609ec30
Qing0111 <a0976151179@gmail.com>
2023-01-20 14:05:04 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: "/vue-final/dist/",
-  // publicPath: process.env.NODE_ENV === 'production'
-  // ? '/vue-final/'
-  // : '/',
+  // publicPath: "/vue-final/dist/",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-final/" : "/",
 });
